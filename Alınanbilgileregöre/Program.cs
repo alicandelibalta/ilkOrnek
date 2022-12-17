91077cb485cc1789441c1dd333692c60f50dfd00
ALICAN <ALICAN@ALD>
2022-12-17 17:03:11 +0300

    Add project files.

---
+++ Alınanbilgileregöre/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int birincisayi, ikincisayi, ucuncusayi, dorduncusayi, besincisayi;
+
+double ortalama;
+
+Console.WriteLine("Lütfen 1. sayıyı gir: ");
+birincisayi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 1. sayıyı gir: ");
+ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 1. sayıyı gir: ");
+ucuncusayi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 1. sayıyı gir: ");
+dorduncusayi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 1. sayıyı gir: ");
+besincisayi = Convert.ToInt32(Console.ReadLine());
+
+ortalama = (birincisayi + ikincisayi + ucuncusayi + dorduncusayi + besincisayi);
+Console.WriteLine("sayıların ortalaması: "+ortalama);
+
+Console.ReadLine();
+
+
+
+
+
---
+++ SelamlamaProgrami/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+
+string isim = "Alican";
+string karşılama = "Bilgeadama hoşgeldiniz.";
+
+int dogumYili=1996;
+int yas = 2022 - dogumYili;
+
+int yil = DateTime.Now.Year
+Console.WriteLine(DateTime.Now.Year);
+
+Console.WriteLine(isim+" "+karşılama);
+Console.WriteLine(isim+" "+dogumYili+" "+"yılında doğmuştur."+" "+isim+" "+yas+" "+"yaşındadır");
+
